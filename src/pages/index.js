6efbf0833a72d3c8309b3892edb77fa187833e71
Yasin Beyazlı <yasinbeyazli29@gmail.com>
2021-09-21 12:06:40 +0300

    Initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Amazon 2.0</title>
+      </Head>
+
+      <h1>Hello</h1>
+    </div>
+  );
+}
